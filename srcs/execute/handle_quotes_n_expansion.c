bb9374eeb5fae5ba5823e8afc858e5aeb633679e
Samuel Ting <sting@u90z02s02.42kl.edu.my>
2024-04-29 16:51:26 +0800

    made changes in expand_var()

--- srcs/execute/handle_quotes_n_expansion.c
+++ srcs/execute/handle_quotes_n_expansion.c
@@ -6,30 +6,40 @@
 /*   By: sting <sting@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 15:57:53 by sting             #+#    #+#             */
-/*   Updated: 2024/04/29 16:08:55 by sting            ###   ########.fr       */
+/*   Updated: 2024/04/29 16:50:16 by sting            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minishell.h"
 
 
-
+/* 
+TODO: search for $, if not found, return str
+TODO: 
+*/
 char *expand_var(char *str, char **my_env)
 {
 	int i;
 	char *result;
+	char *value;
 
+	if (ft_strchr(str, '$') == NULL)
+		return (str); // no $ found, return str
 	i = -1;
 	while (str[++i])
 	{
 		if (str[i] == '$')
 		{
-			
+			// store value
+			value = my_getenv(&str[i + 1], my_env);
+			if (value == NULL)
+				value = ""; // ! if var not found in my_env, expand to ""
 		}
 		
 	}
 	
-	free(str); // TODO: only free if str is changed
+	// result = strjoin(str before $, value);
+	free(str);
 	return (result);
 }
 
@@ -47,8 +57,11 @@ int handle_quotes_n_var_expansion(char **cmd, char **my_env)
 		if (cmd[i][0] == '\'') // single quote
 			expand = OFF;
 		if (cmd[i][0] == '\'' || cmd[i][0] == '\"') 
+		{
 			tmp = ft_strtrim(cmd[i], cmd[i][0]);
-		
+			if (tmp == NULL)
+				perror_and_exit("ft_strtrim", EXIT_FAILURE);
+		}
 		if (expand == ON);
 			tmp = expand_var(tmp, my_env); // TODO ,search for $, remalloc, free old str
 			
