eedbc8d76b20a5b544efea8f398feb79d363c6d2
Samuel Ting <sting@u90z02s02.42kl.edu.my>
2024-05-03 16:49:49 +0800

    working on var_expansion: edge case: HOME

--- srcs/execute/handle_quotes_n_expansion.c
+++ srcs/execute/handle_quotes_n_expansion.c
@@ -57,13 +57,103 @@ free the old one
 // 	// free(str); // ! don't free because str is a copy?
 // }
 
+// void expand_str(char **str_add, char **my_env)
+// {
+// 	char *str;
+// 	int i;
+// 	char *value;
+// 	char *substr_front;
+// 	// char *substr_back;
+// 	char *result;
+
+// 	str = *str_add;
+// 	if (ft_strchr(*str_add, '$') == NULL)
+// 		return ;
+// 	i = -1;
+// 	while (str[++i]) // find index of $
+// 	{
+// 		if (str[i] == '$' && str[i + 1] != '\0') 
+// 		{
+// 			// store value
+// 			 // TODO: create substr(for var_name) that ends when encounter non-alnum or \0
+// 			value = my_getenv(&str[i + 1], my_env);
+// 			if (value == NULL)
+// 				value = ""; // ! if var not found in my_env, expand to ""
+
+// 			substr_front = ft_substr(str, 0, i);
+// 			if_null_perror_n_exit(substr_front, "ft_substr", EXIT_FAILURE);
+// 			result = ft_strjoin(substr_front, value);
+// 			if_null_perror_n_exit(result, "ft_strjoin", EXIT_FAILURE);
+// 			free(substr_front);	
+			
+// 			// save chars behind var_name
+// 			// while (str[i] && ft_isalnum(str[i++])); // find index of non alnum chars after var_name
+// 			// substr_back = ft_substr(str, i, ft_strlen(str));
+// 			free(*str_add);
+// 			*str_add = result;
+// 			return ;
+// 		}
+// 	}
+// }
+
+// void expand_str(char **str_add, char **my_env)
+// {
+// 	int i;
+// 	int j;
+// 	char *str;
+// 	char *value;
+// 	char *substr_front;
+// 	char *var_name;
+// 	char *substr_back;
+// 	char *result;
+
+// 	str = *str_add;
+// 	if (ft_strchr(*str_add, '$') == NULL)
+// 		return ;
+// 	i = -1;
+// 	while (str[++i]) // find index of $
+// 	{
+// 		if (str[i] == '$' && str[i + 1] != '\0') 
+// 		{
+// 			// store value
+// 			// TODO: create substr(for var_name) that ends when encounter non-alnum or \0
+			
+// 			j = i + 1;
+// 			while (str[j] && ft_isalnum(str[j]))
+// 				j++; // find index of non alnum chars after var_name
+// 			var_name = ft_substr(str, i, j - i); 
+
+// 			value = my_getenv(var_name, my_env);
+// 			if (value == NULL)
+// 				value = ""; // ! if var not found in my_env, expand to ""
+// 			substr_front = ft_substr(str, 0, i);
+// 			if_null_perror_n_exit(substr_front, "ft_substr", EXIT_FAILURE);
+			
+// 			// save chars behind var_name
+// 			substr_back = ft_substr(str, j, ft_strlen(str) - j);
+// 			if_null_perror_n_exit(substr_back, "ft_substr", EXIT_FAILURE);
+
+// 			result = ft_strjoin(substr_front, value);
+// 			if_null_perror_n_exit(result, "ft_strjoin", EXIT_FAILURE);
+// 			free(substr_front);	
+
+// 			char *result2 = ft_strjoin(result, substr_back);
+// 			if_null_perror_n_exit(result, "ft_strjoin", EXIT_FAILURE);
+
+// 			free(*str_add);
+// 			*str_add = result2;
+// 			return ;
+// 		}
+// 	}
+// }
+
 void expand_str(char **str_add, char **my_env)
 {
+	int i; // index of $ sign
+	int j; // index of non-alnum char right after var_name
 	char *str;
-	int i;
 	char *value;
-	char *substr_front;
-	// char *substr_back;
+	char *var_name;
 	char *result;
 
 	str = *str_add;
@@ -74,21 +164,21 @@ void expand_str(char **str_add, char **my_env)
 	{
 		if (str[i] == '$' && str[i + 1] != '\0') 
 		{
-			// store value
-			// TODO: sotre
-			value = my_getenv(&str[i + 1], my_env);
+			j = i + 1;
+			while (str[j] && ft_isalnum(str[j]))
+				j++; // find index of non alnum chars after var_name
+			var_name = ft_substr(str, i + 1, j - i); // ! stopped here friday may 3rd
+			printf("var_name: %s\n", var_name);
+			value = my_getenv(var_name, my_env);
 			if (value == NULL)
 				value = ""; // ! if var not found in my_env, expand to ""
-
-			substr_front = ft_substr(str, 0, i);
-			if_null_perror_n_exit(substr_front, "ft_substr", EXIT_FAILURE);
-			result = ft_strjoin(substr_front, value);
-			if_null_perror_n_exit(result, "ft_strjoin", EXIT_FAILURE);
-			free(substr_front);	
-			
-			// save chars behind var_name
-			// while (str[i] && ft_isalnum(str[i++])); // find index of non alnum chars after var_name
-			// substr_back = ft_substr(str, i, ft_strlen(str));
+			int size = i + ft_strlen(value) + ft_strlen(str) - j;
+			result = (char *)malloc(sizeof(char) * size + 1);
+			if_null_perror_n_exit(result, "malloc", EXIT_FAILURE);
+			ft_strlcpy(result, str, i);
+			ft_strlcpy(&result[i], value, ft_strlen(value));
+			ft_strlcpy(&result[i + ft_strlen(value)], &str[j], ft_strlen(str) - j);
+			result[size] = '\0';
 			free(*str_add);
 			*str_add = result;
 			return ;
