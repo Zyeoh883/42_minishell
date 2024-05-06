37f79dd6d616495c3b631e92a387a60e518fa155
Samuel Ting <sting@u90z02s02.42kl.edu.my>
2024-05-06 13:50:41 +0800

    completed: extraction of var_name based on set of rules (for variable expansion)

--- srcs/execute/handle_quotes_n_expansion.c
+++ srcs/execute/handle_quotes_n_expansion.c
@@ -6,106 +6,21 @@
 /*   By: sting <sting@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 15:57:53 by sting             #+#    #+#             */
-/*   Updated: 2024/05/02 15:49:16ting            ###   ########.fr       */
+/*   Updated: 2024/05/06 13:42:45 by sting            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "minishell.h"
-/*
-	- replaces original location of str with trimmed/expanded str
-	- free old str
-*/
-void update_ori_cmd_array(char **str, char *updated_str)
-{
-	free(*str);
-	str = &updated_str;
-}
-
-/* 
-TODO: NOTDON -> Takes in address of str, replaces the location with a new str, 
-free the old one
-*/
-// char *return_expanded_str(char *str, char **my_env)
-// {
-// 	int i;
-// 	char *result;
-// 	char *value;
-// 	char *substr;
-	
-// 	if (str == NULL) // *testing
-// 		printf("str is NULL\n");
-// 	if (ft_strchr(str, '$') == NULL)
-// 		return (str); // no $ found, return str
-// 	i = -1;
-// 	while (str[++i]) // find index of $
-// 	{
-// 		if (str[i] == '$' && str[i + 1] != '\0') 
-// 		{
-// 			// store value
-// 			value = my_getenv(&str[i + 1], my_env);
-// 			if (value == NULL)
-// 				value = ""; // ! if var not found in my_env, expand to ""
-// 			substr = ft_substr(str, 0, i);
-// 			if_null_perror_n_exit(substr, "ft_substr", EXIT_FAILURE);
-// 			result = ft_strjoin(substr, value);
-// 			if_null_perror_n_exit(result, "ft_strjoin", EXIT_FAILURE);
-// 			free(substr);
-// 			return (result);
-// 		}
-// 	}
-// 	return(str);
-// 	// free(str); // ! don't free because str is a copy?
-// }
+#include "minishell.h"
 
 // void expand_str(char **str_add, char **my_env)
 // {
-// 	char *str;
-// 	int i;
-// 	char *value;
-// 	char *substr_front;
-// 	// char *substr_back;
-// 	char *result;
-
-// 	str = *str_add;
-// 	if (ft_strchr(*str_add, '$') == NULL)
-// 		return ;
-// 	i = -1;
-// 	while (str[++i]) // find index of $
-// 	{
-// 		if (str[i] == '$' && str[i + 1] != '\0') 
-// 		{
-// 			// store value
-// 			 // TODO: create substr(for var_name) that ends when encounter non-alnum or \0
-// 			value = my_getenv(&str[i + 1], my_env);
-// 			if (value == NULL)
-// 				value = ""; // ! if var not found in my_env, expand to ""
-
-// 			substr_front = ft_substr(str, 0, i);
-// 			if_null_perror_n_exit(substr_front, "ft_substr", EXIT_FAILURE);
-// 			result = ft_strjoin(substr_front, value);
-// 			if_null_perror_n_exit(result, "ft_strjoin", EXIT_FAILURE);
-// 			free(substr_front);	
-			
-// 			// save chars behind var_name
-// 			// while (str[i] && ft_isalnum(str[i++])); // find index of non alnum chars after var_name
-// 			// substr_back = ft_substr(str, i, ft_strlen(str));
-// 			free(*str_add);
-// 			*str_add = result;
-// 			return ;
-// 		}
-// 	}
-// }
-
-// void expand_str(char **str_add, char **my_env)
-// {
-// 	int i;
-// 	int j;
+// 	int i; // index of $ sign
+// 	int j; // index of non-alnum char right after var_name
+// 	int size;
 // 	char *str;
 // 	char *value;
-// 	char *substr_front;
 // 	char *var_name;
-// 	char *substr_back;
-// 	char *result;
+// 	char *expanded;
 
 // 	str = *str_add;
 // 	if (ft_strchr(*str_add, '$') == NULL)
@@ -113,83 +28,87 @@ free the old one
 // 	i = -1;
 // 	while (str[++i]) // find index of $
 // 	{
-// 		if (str[i] == '$' && str[i + 1] != '\0') 
+// 		if (str[i] == '$' && str[i + 1] != '\0')
 // 		{
-// 			// store value
-// 			// TODO: create substr(for var_name) that ends when encounter non-alnum or \0
-			
 // 			j = i + 1;
 // 			while (str[j] && ft_isalnum(str[j]))
 // 				j++; // find index of non alnum chars after var_name
-// 			var_name = ft_substr(str, i, j - i); 
-
+// 			var_name = ft_substr(str, i + 1, j - i - 1);
 // 			value = my_getenv(var_name, my_env);
 // 			if (value == NULL)
 // 				value = ""; // ! if var not found in my_env, expand to ""
-// 			substr_front = ft_substr(str, 0, i);
-// 			if_null_perror_n_exit(substr_front, "ft_substr", EXIT_FAILURE);
-			
-// 			// save chars behind var_name
-// 			substr_back = ft_substr(str, j, ft_strlen(str) - j);
-// 			if_null_perror_n_exit(substr_back, "ft_substr", EXIT_FAILURE);
-
-// 			result = ft_strjoin(substr_front, value);
-// 			if_null_perror_n_exit(result, "ft_strjoin", EXIT_FAILURE);
-// 			free(substr_front);	
-
-// 			char *result2 = ft_strjoin(result, substr_back);
-// 			if_null_perror_n_exit(result, "ft_strjoin", EXIT_FAILURE);
-
+// 			size = i + ft_strlen(value) + ft_strlen(str) - j;
+// 			expanded = (char *)malloc(sizeof(char) * (size + 1));
+// 			if_null_perror_n_exit(expanded, "malloc", EXIT_FAILURE);
+// 			ft_strlcpy(expanded, str, i + 1); // +1 as 3rd param includes
+// 			ft_strlcpy(&expanded[i], value, ft_strlen(value) + 1);
+// 			ft_strlcpy(&expanded[i + ft_strlen(value)], &str[j], ft_strlen(str)
+// 				- j + 1);
+// 			expanded[size] = '\0';
 // 			free(*str_add);
-// 			*str_add = result2;
+// 			*str_add = expanded;
 // 			return ;
 // 		}
 // 	}
 // }
 
-void expand_str(char **str_add, char **my_env)
+int	get_index_of_char_after_var_name(char *str, int dollar_index)
 {
-	int i; // index of $ sign
-	int j; // index of non-alnum char right after var_name
-	int size;
-	char *str;
-	char *value;
-	char *var_name;
-	char *result;
+	int j;
+	
+	j = dollar_index + 1;
+	if (ft_isalpha(str[j]))
+	{
+		while (str[j] && ft_isalnum(str[j]))
+			j++; // find index of non alnum chars after var_name
+		return (j);
+	}
+	else // if var_name starts number/symbol
+		return (j + 1);
+}
+
+/*
+	int		i; -> index of $ sign
+	int		j; -> index of char right after var_name
+*/
+void	expand_str(char **str_add, char **my_env)
+{
+	int		size;
+	char	*str;
+	char	*value;
+	char	*var_name;
+	char	*expanded;
+	int		i;
+	int		j;
 
 	str = *str_add;
 	if (ft_strchr(*str_add, '$') == NULL)
 		return ;
 	i = -1;
-	while (str[++i]) // find index of $
-	{
-		if (str[i] == '$' && str[i + 1] != '\0') 
-		{
-			j = i + 1;
-			while (str[j] && ft_isalnum(str[j]))
-				j++; // find index of non alnum chars after var_name
-			var_name = ft_substr(str, i + 1, j - i - 1); // ! stopped here friday may 3rd
-			value = my_getenv(var_name, my_env);
-			if (value == NULL)
-				value = ""; // ! if var not found in my_env, expand to ""
-			size = i + ft_strlen(value) + ft_strlen(str) - j;
-			result = (char *)malloc(sizeof(char) * (size + 1));
-			if_null_perror_n_exit(result, "malloc", EXIT_FAILURE);
-			ft_strlcpy(result, str, i + 1); // +1 as 3rd param includes 
-			ft_strlcpy(&result[i], value, ft_strlen(value) + 1);
-			ft_strlcpy(&result[i + ft_strlen(value)], &str[j], ft_strlen(str) - j + 1);
-			result[size] = '\0';
-			free(*str_add);
-			*str_add = result;
-			return ;
-		}
-	}
+	while (str[++i] && !(str[i] == '$' && str[i + 1] != '\0'))
+		; // find index of $
+	if (str[i] == '\0')
+		return ;
+	j = get_index_of_char_after_var_name(str, i);
+	var_name = ft_substr(str, i + 1, j - i - 1);
+	value = my_getenv(var_name, my_env);
+	if (value == NULL)
+		value = ""; // ! if var not found in my_env, expand to ""
+	size = i + ft_strlen(value) + ft_strlen(str) - j;
+	expanded = (char *)malloc(sizeof(char) * (size + 1));
+	if_null_perror_n_exit(expanded, "malloc", EXIT_FAILURE);
+	ft_strlcpy(expanded, str, i + 1); // +1 as 3rd param includes
+	ft_strlcpy(&expanded[i], value, ft_strlen(value) + 1);
+	ft_strlcpy(&expanded[i + ft_strlen(value)], &str[j], ft_strlen(str) - j
+		+ 1);
+	expanded[size] = '\0';
+	free(*str_add);
+	*str_add = expanded;
 }
 
-// quote_type: "\'" OR "\""
-void trim_quotes(char **str_add, char *quote_type)
+void	trim_quotes(char **str_add, char *quote_type)
 {
-	char *trimmed_str;
+	char	*trimmed_str;
 
 	trimmed_str = ft_strtrim(*str_add, quote_type);
 	if_null_perror_n_exit(trimmed_str, "ft_strtrim", EXIT_FAILURE);
@@ -197,19 +116,16 @@ void trim_quotes(char **str_add, char *quote_type)
 	*str_add = trimmed_str;
 }
 
-void handle_quotes_n_var_expansion(char **cmd, char **my_env)
+void	handle_quotes_n_var_expansion(char **cmd, char **my_env)
 {
-	// char *trimmed_str = NULL;
-	// char *expanded_str = NULL;
-	int i;
-	int expand;
-	
+	int	i;
+	int	expand;
+
 	(void)my_env;
 	i = -1;
-	
 	while (cmd[++i])
 	{
-		expand = ON; // ON by default
+		expand = ON;           // ON by default
 		if (cmd[i][0] == '\'') // single quote
 		{
 			expand = OFF;
@@ -217,14 +133,7 @@ void handle_quotes_n_var_expansion(char **cmd, char **my_env)
 		}
 		else if (cmd[i][0] == '\"') // double quote
 			trim_quotes(&cmd[i], "\"");
-
-		// printf("trimmed: %s\n", cmd[i]);
 		if (expand == ON)
 			expand_str(&cmd[i], my_env);
-		// printf("expanded: %s\n", cmd[i]);
-
-		// 	expanded_str = return_expanded_str(trimmed_str, my_env);
-		// free(trimmed_str);	// ! double free: as return_expanded_str could be the original str->"trimmed_str" if there's no $
-		// update_ori_cmd_array(&cmd[i], expanded_str); // TODO, remalloc & free
 	}
 }
