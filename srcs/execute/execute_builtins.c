02c5c60116110b3dc0b22d06cd622b8dc7a3a9a7
Samuel Ting <sting@u90z02s02.42kl.edu.my>
2024-05-23 17:30:52 +0800

    working on handling issue with printing spaces in echo

--- srcs/execute/execute_builtins.c
+++ srcs/execute/execute_builtins.c
@@ -6,11 +6,12 @@
 /*   By: sting <sting@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 13:04:50 by sting             #+#    #+#             */
-/*   Updated: 2024/05/23 13:40:26 by sting            ###   ########.fr       */
+/*   Updated: 2024/05/23 17:30:01 by sting            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
 /*
 int j;  j -> index of '='
 */
@@ -38,14 +39,12 @@ int	execute_export(char **cmd_arg, t_var *var_lst)
 			if (!(ft_isalnum(cmd_arg[i][j]) || cmd_arg[i][j] == '_'))
 				return (print_custom_err_n_return("export: `", cmd_arg[i],
 						"\': not a valid identifier", EXIT_FAILURE));
-						
 		// search for var_name, check if valid
 		var_name = ft_substr(cmd_arg[i], 0, j);
 		if_null_perror_n_exit(var_name, "ft_substr", EXIT_FAILURE);
 		printf("(export) var_name: %s\n", var_name); // !remove
 		node = get_var_node(var_name, var_lst);
 		free(var_name);
-		
 		if (node != NULL) // if variable exist
 		{
 			node->is_exported = true;
@@ -103,22 +102,46 @@ void	execute_pwd(void)
 }
 
 // ! need to return int for failure?
+// void	execute_echo(char **cmd_arg)
+// {
+// 	int	i;
+// 	int	n_flag;
+
+// 	n_flag = OFF;
+// 	if (cmd_arg[1] && ft_strcmp(cmd_arg[1], "-n") == 0)
+// 		n_flag = ON;
+// 	i = 1;
+// 	if (n_flag == ON)
+// 		i = 2;
+// 	while (cmd_arg[i] != NULL)
+// 	{
+// 		ft_putstr_fd(cmd_arg[i], STDOUT_FILENO);
+// 		if (cmd_arg[i + 1] != NULL)
+// 			ft_putstr_fd(" ", STDOUT_FILENO);
+// 		i++;
+// 	}
+// 	if (n_flag == OFF)
+// 		ft_putstr_fd("\n", STDOUT_FILENO);
+// }
+
 void	execute_echo(char **cmd_arg)
 {
 	int	i;
 	int	n_flag;
 
 	n_flag = OFF;
-	if (cmd_arg[1] && ft_strlen(cmd_arg[1]) == 2 && ft_strcmp(cmd_arg[1],
-			"-n") == 0)
-		n_flag = ON;
 	i = 1;
-	if (n_flag == ON)
-		i = 2;
 	while (cmd_arg[i] != NULL)
 	{
+		if ((i == 1 || (i > 1 && cmd_arg[i - 1][0] == '\0'))
+			&& ft_strcmp(cmd_arg[i], "-n") == 0)
+		{
+			n_flag = ON;
+			i++;
+			continue ;
+		}
 		ft_putstr_fd(cmd_arg[i], STDOUT_FILENO);
-		if (cmd_arg[i + 1] != NULL)
+		if (cmd_arg[i + 1] != NULL && // ! INSERT code in notion under "solution 2")
 			ft_putstr_fd(" ", STDOUT_FILENO);
 		i++;
 	}
