af9c55241910e05afee2dbf777494cf1ab279a5e
samueltingg <samuelting7@gmail.com>
2024-06-05 13:42:55 +0800

    started on execute_assignment()

--- srcs/execute/builtins/execute_assignment.c
+++ srcs/execute/builtins/execute_assignment.c
@@ -6,11 +6,16 @@
 /*   By: sting <sting@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:49:16 by sting             #+#    #+#             */
-/*   Updated: 2024/06/05 13:10:27 by sting            ###   ########.fr       */
+/*   Updated: 2024/06/05 13:33:49 by sting            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int is_assignment(char **cmd_arg)
+{
+	
+}
 
 int execute_assignment(char **cmd_arg, t_var *var_lst)
 {
@@ -18,13 +23,11 @@ int execute_assignment(char **cmd_arg, t_var *var_lst)
 	int	equal_index;
 
 	printf(">>>>>BUILT_IN>>>>>\n");
-	i = 0;
+	// CHECK if all assignments are valid, if 1 invalid, ALL cancelled
+	i = -1;
 	while (cmd_arg[++i] != NULL)
-	{
-		equal_index = count_chars_until_equal_sign(cmd_arg[i]);
 		if (is_var_name_valid(cmd_arg[i]) == EXIT_FAILURE)
-			return (EXIT_FAILURE);
-		update_or_add_variable(var_lst, cmd_arg, i, equal_index);
-	}
+			return (NOT_BUILTIN);
+	// i = 0;
 	return (EXIT_SUCCESS);
 }
