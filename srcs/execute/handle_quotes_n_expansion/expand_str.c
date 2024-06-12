868ffff7e3d959d8bbb8a0f477e99f0c5893184a
samueltingg <samuelting7@gmail.com>
2024-06-12 12:40:55 +0800

    chore: minor changes to variable names in expand_str.c

--- srcs/execute/handle_quotes_n_expansion/expand_str.c
+++ srcs/execute/handle_quotes_n_expansion/expand_str.c
@@ -6,7 +6,7 @@
 /*   By: sting <sting@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 12:20:09 by sting             #+#    #+#             */
-/*   Updated: 2024/06/12 12:23:40 by sting            ###   ########.fr       */
+/*   Updated: 2024/06/12 12:40:18 by sting            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,52 +35,52 @@ int	get_index_of_char_after_var_name(char *str, int dollar_index)
 	int		d; -> index of $ sign
 	int		c; -> index of char right after var_name
 */
-char	*construct_expanded_str(char **str, char *value, int d, int c)
+char	*construct_expanded_str(char *str, char *value, int d, int c)
 {
 	int		size;
 	char	*expanded;
 
-	size = d + ft_strlen(value) + (ft_strlen((*str)) - c);
+	size = d + ft_strlen(value) + (ft_strlen((str)) - c);
 	expanded = (char *)ft_calloc(size + 1, sizeof(char));
 	if_null_perror_n_exit(expanded, "ft_calloc", EXIT_FAILURE);
-	ft_strlcpy(expanded, (*str), d + 1);
+	ft_strlcpy(expanded, (str), d + 1);
 	ft_strlcpy(&expanded[d], value, ft_strlen(value) + 1);
-	ft_strlcpy(&expanded[d + ft_strlen(value)], &(*str)[c], ft_strlen(*str) - c
-		+ 1);
+	ft_strlcpy(&expanded[d + ft_strlen(value)], &(str)[c],
+		ft_strlen(str) - c + 1);
 	return (expanded);
 }
 
 // returns index of char right after expanded value
-int	expand_single_var(char **str, int dollar_index, t_var *var_lst)
+int	expand_single_var(char **str_add, int dollar_index, t_var *var_lst)
 {
-	int		j;
+	int		c;
 	char	*var_name;
 	char	*value;
 	char	*expanded;
 
-	j = get_index_of_char_after_var_name(*str, dollar_index);
-	var_name = ft_substr(*str, (dollar_index + 1), (j - dollar_index - 1));
+	c = get_index_of_char_after_var_name(*str_add, dollar_index);
+	var_name = ft_substr(*str_add, (dollar_index + 1), (c - dollar_index - 1));
 	if_null_perror_n_exit(var_name, "ft_substr", EXIT_FAILURE);
 	value = get_var_value(var_name, var_lst);
 	if (value == NULL)
 		value = "\0";
 	free(var_name);
-	expanded = construct_expanded_str(str, value, dollar_index, j);
-	free(*str);
-	*str = expanded;
+	expanded = construct_expanded_str(*str_add, value, dollar_index, c);
+	free(*str_add);
+	*str_add = expanded;
 	return (dollar_index + ft_strlen(value));
 }
 
-// str -> address of str
-void	expand_str(char **str, t_var *var_lst)
+// str_add -> address of str
+void	expand_str(char **str_add, t_var *var_lst)
 {
 	int	i;
+	char *str;
 
-	i = 0;
-	while ((*str)[i])
+	while ((*str_add)[i])
 	{
-		if ((*str)[i] == '$' && (*str)[i + 1] != '\0')
-			i = expand_single_var(str, i, var_lst);
+		if ((*str_add)[i] == '$' && (*str_add)[i + 1] != '\0')
+			i = expand_single_var(str_add, i, var_lst);
 		else
 			i++;
 	}
