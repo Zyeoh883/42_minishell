708202cc3f778dd3e184c7df37230185b6cb5121
samueltingg <samuelting7@gmail.com>
2024-06-26 10:27:58 +0800

    feat: norm-ed execute_echo.c

--- srcs/execute/builtins/execute_echo.c
+++ srcs/execute/builtins/execute_echo.c
@@ -6,13 +6,23 @@
 /*   By: sting <sting@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 14:59:30 by sting             #+#    #+#             */
-/*   Updated: 2024/06/25 16:47:08 by sting            ###   ########.fr       */
+/*   Updated: 2024/06/26 10:21:23 by sting            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// ! NORM
+void	print_args_separated_by_spaces(char **arg)
+{
+	while (*arg != NULL)
+	{
+		ft_putstr_fd(*arg, STDOUT_FILENO);
+		if (*(arg + 1) != NULL)
+			ft_putstr_fd(" ", STDOUT_FILENO);
+		arg++;
+	}
+}
+
 int	execute_echo(t_simple_command *sc)
 {
 	pid_t	pid;
@@ -28,19 +38,13 @@ int	execute_echo(t_simple_command *sc)
 		n_flag = OFF;
 		if (sc->cmd_arg[1] && ft_strcmp(sc->cmd_arg[1], "-n") == 0)
 			n_flag = ON;
-		i = 0;
+		i = 1;
 		if (n_flag == ON)
 			i++;
-		while (sc->cmd_arg[++i] != NULL)
-		{
-			ft_putstr_fd(sc->cmd_arg[i], STDOUT_FILENO);
-			if (sc->cmd_arg[i + 1] != NULL)
-				ft_putstr_fd(" ", STDOUT_FILENO);
-		}
+		print_args_separated_by_spaces(&sc->cmd_arg[i]);
 		if (n_flag == OFF)
 			ft_putstr_fd("\n", STDOUT_FILENO);
 		exit(EXIT_SUCCESS);
 	}
 	return (waitpid_n_get_exit_status(pid));
 }
-
